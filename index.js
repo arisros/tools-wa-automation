8580ca966af96592974d25032c083008a8338e22
Aris Jirat Kurniawan <arisjiratkurniawan@gmail.com>
2020-10-27 15:50:27 +0700

    add validation and csv logs failure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const wa = require('@open-wa/wa-automate');
 const csvtojson = require("csvtojson/v2");
 const fsRenameSync = require('fs').renameSync;
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const prefixOldFiles = 'PaySlip_September_-';
 const sessionId = 'newSessionId'
@@ -28,6 +29,7 @@ const Boot = async () => {
   const client = await wa.create(config)
   try {
     const contacts = await getAllContacts();
+    await contactValidation(client, contacts);
     const contactWithFiles = await renameFilesFromContacts(contacts);
     startWA(client, contactWithFiles);
   } catch (error) {
@@ -35,6 +37,8 @@ const Boot = async () => {
     client.kill();
   }
 }
+
+
 
 const renameFilesFromContacts = async (contacts) => {
   const contactWithFiles = contacts.map(async (c, i) => {
@@ -49,30 +53,89 @@ const renameFilesFromContacts = async (contacts) => {
   return Promise.all(contactWithFiles)
 }
 
+const contactValidation = async (client, contacts) => {
+  let contactInvalid = [];
+  try {
+    for (let index = 0; index < contacts.length; index++) {
+      const contact = contacts[index];
+      const details = await client.getContact(`${contact.Phone}@c.us`) || {}
+      const detailName = details.name || null
+      if (detailName !== contact.Name) {
+        contactInvalid.push({
+          name: contact.Name,
+          phone: contact.Phone,
+          fromDetailContact: details.name ? JSON.stringify(details) : 'Contact Not found'
+        })
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    client.kill()
+  }
+  if (contactInvalid.length) {
+    createLogCSV(contactInvalid, 'invalid-contact')
+    throw new Error('Ada kontak yang tidak sama dengan daftar contacts/all.csv');
+  }
+  console.log('All Contact Are Valid!')
+  return true
+}
+
 const startWA = async (client, contacts) => {
   const synced = await client.syncContacts()
   const own = await client.getMe()
   console.log(own.status, own.pushname)
-  console.log('==SYNCED CONTACT==', synced)
-  console.log('==START SEND CHAT==');
+  console.log('==Contact Synced==', synced)
+  let notSended = []
   try {
     for (let index = 0; index < contacts.length; index++) {
       const contact = contacts[index];
-      await client.sendFile(
+      const send = await client.sendFile(
         `${contact.Phone}@c.us`, //chatId
         `${contact.file}`, // filePath
         `${contact.Phone}.${contact.Name}.pdf`, //filename
         `Paylip Kamu ${contact.Name}` //caption
       );
-      console.log('==SUCCESS SEND FILE==');
+      if (!send) {
+        notSended.push({ contact })
+      }
+      console.log(`==Success send file to ${contact.Name} - ${contact.Phone}==`);
     }
   } catch (error) {
-    console.log('SEND ERR!!: Cause', error);
+    if (notSended.length) {
+      createLogCSV(notSended, 'failed-send')
+    }
+    console.log('Send ERR!!: Cause', error);
     client.kill();
   }
-
-  console.log('==END SEND CHAT==');
+  if (notSended.length) {
+    createLogCSV(notSended, 'failed-send')
+  }
+  console.log('==Destroy==');
   client.kill()
+}
+
+const generateHeader = rows => {
+  if (!rows.length) {
+    throw new Error('Tidak ada Data untuk mendapatkan header')
+  }
+  const header = Object.keys(rows[0]).map(e => ({
+    id: e,
+    title: e.toUpperCase()
+  }))
+  return header
+}
+
+const createLogCSV = (rows, name) => {
+  const now = new Date();
+  const dateFormat = `${now.getMonth()}-${now.getDate()}-${now.getFullYear()}:${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
+  const csvWriter = createCsvWriter({
+    path: `./errors/${name || 'log'}-${dateFormat}.csv`,
+    header: generateHeader(rows)
+  });
+  
+  csvWriter
+    .writeRecords(rows)
+    .then(()=> console.log(`Log ${name} CSV file was written successfully`));
 }
 
 
